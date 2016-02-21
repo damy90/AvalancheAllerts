92e0cc7f4e52f2197d6a79ee8fa5ae90a3109c1f
damy90 <dany90@gmail.com>
2016-02-21 20:05:41 +0200

    Adding TestsController and TestViewModel

---
+++ Source/Web/AvalancheAllerts.Web/Controllers/TestsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvalancheAllerts.Web.Controllers
+{
+    using System.Device.Location;
+
+    using AvalancheAllerts.Services.Data;
+    using AvalancheAllerts.Web.Infrastructure.Mapping;
+    using AvalancheAllerts.Web.ViewModels;
+
+    public class TestsController : Controller
+    {
+        private readonly ITestsService Tests;
+
+        public TestsController(ITestsService tests)
+        {
+            this.Tests = tests;
+        }
+
+        // GET: Tests
+        public ActionResult GetAll()
+        {
+            var result = this.Tests.GetAll().Where(t => t.IsDeleted == false).To<TestViewModel>().ToList();
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Nearby(double lat, double lon, int radius)
+        {
+            var currentPosition = new GeoCoordinate(lat, lon);
+            var result = this.Tests.FilterRadius(currentPosition, radius * 1000).Where(t => t.IsDeleted == false).To<TestViewModel>().ToList(); //to meters
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Source/Web/AvalancheAllerts.Web/ViewModels/TestViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace AvalancheAllerts.Web.ViewModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Device.Location;
+
+    using AvalancheAllerts.Data.Models;
+    using AvalancheAllerts.Web.Infrastructure.Mapping;
+
+    public class TestViewModel : IMapFrom<Test>
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
+        public string Place { get; set; }
+
+        public GeoCoordinate Position { get; set; }
+
+        [Required]
+        public string TestResultsDescription { get; set; }
+
+        [Range(1, 5)]
+        public int DangerLevel { get; set; }
+
+        //TODO:username
+    }
+}

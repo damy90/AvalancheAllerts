d9d4597fdc5d6ae758ade124b3f0ed12bd343331
damy90 <dany90@gmail.com>
2016-02-22 15:48:03 +0200

    Moved non admin functionality outside of the admon area

--- Source/Web/AvalancheAllerts.Web/Areas/Administration/Controllers/OrganisationsAdminController.cs
+++ Source/Web/AvalancheAllerts.Web/Areas/Administration/Controllers/OrganisationsAdminController.cs
@@ -20,108 +20,11 @@ namespace AvalancheAllerts.Web.Areas.Administration.Controllers
 
     public class OrganisationsAdminController : BaseController
     {
-        //private ApplicationDbContext db = new ApplicationDbContext();
-
         private readonly IOrganisationsService Organisations;
 
         public OrganisationsAdminController(IOrganisationsService organisations)
         {
             this.Organisations = organisations;
-        }
-
-        // GET: Administration/OrganisationsAdmin
-        public ActionResult Index()
-        {
-            var organisations = this.Organisations.GetAll()
-                .Where(x => x.IsDeleted == false)
-                .To<OrganisationViewModel>()
-                .ToList();
-
-            return View(organisations);
-        }
-
-        // GET: Administration/OrganisationsAdmin/Details/5
-        public ActionResult Details(int id)
-        {
-            var organisation = this.Organisations.GetAll()//.ToList().AsQueryable()
-                .To<OrganisationDetailsModel>()
-                .FirstOrDefault(x => x.Id == id);
-            if (organisation == null)
-            {
-                return this.HttpNotFound();
-            }
-            return this.View(organisation);
-        }
-
-        // GET: Administration/OrganisationsAdmin/Create
-        public ActionResult Create()
-        {
-            //ViewBag.OwnerId = new SelectList(db.Users, "Id", "Email");
-            return View();
-        }
-
-        // POST: Administration/OrganisationsAdmin/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public ActionResult Create([Bind(Include = "Id,Name,Description,OwnerId,CreatedOn,ModifiedOn,IsDeleted,DeletedOn")] OrganisationCreateModel organisation)
-        {
-            if (ModelState.IsValid)
-            {
-                var entity = new Organisation()
-                {
-                    Name = organisation.Name,
-                    Description = organisation.Description,
-                    OwnerId = this.User.Identity.GetUserId()
-                };
-
-                this.Organisations.Add(entity);
-                this.Organisations.SaveChanges();
-                return this.RedirectToAction("Index");
-            }
-
-            //ViewBag.OwnerId = new SelectList(db.Users, "Id", "Email", organisation.OwnerId);
-            return View(organisation);
-        }
-
-        // GET: Administration/OrganisationsAdmin/Edit/5
-        public ActionResult Edit(int id)
-        {
-            var organisation = this.Organisations.GetAll().To<OrganisationViewModel>().FirstOrDefault(o => o.Id == id);
-
-            if (organisation == null)
-            {
-                return HttpNotFound();
-            }
-
-            //ViewBag.OwnerId = new SelectList(db.Users, "Id", "Email", organisation.OwnerId);
-            return View(organisation);
-        }
-
-        // POST: Administration/OrganisationsAdmin/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Description,OwnerId,CreatedOn,ModifiedOn,IsDeleted,DeletedOn")] OrganisationEditModel organisation)
-        {
-            if (ModelState.IsValid)
-            {
-                var entity = this.Organisations.GetById(organisation.Id);
-                entity.Description = organisation.Description;
-                entity.Name = organisation.Name;
-
-                this.Organisations.Update(entity);
-                this.Organisations.SaveChanges();
-                return this.RedirectToAction("Index");
-            }
-
-            var result =
-                this.Organisations.GetAll().To<OrganisationViewModel>().FirstOrDefault(o => o.Id == organisation.Id);
-
-            return this.View(result);
         }
 
         // GET: Administration/OrganisationsAdmin/Delete/5
@@ -144,7 +47,7 @@ namespace AvalancheAllerts.Web.Areas.Administration.Controllers
             Organisation organisation = this.Organisations.GetById(id);
             this.Organisations.Delete(organisation);
             this.Organisations.SaveChanges();
-            return this.RedirectToAction("Index");
+            return this.RedirectToAction("Index", "Organisations");
         }
 
         /*protected override void Dispose(bool disposing)
---
+++ Source/Web/AvalancheAllerts.Web/Controllers/OrganisationsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvalancheAllerts.Web.Controllers
+{
+    using AvalancheAllerts.Data.Models;
+    using AvalancheAllerts.Services.Data;
+    using AvalancheAllerts.Web.Infrastructure.Mapping;
+    using AvalancheAllerts.Web.ViewModels.Organisation;
+
+    using Microsoft.AspNet.Identity;
+
+    public class OrganisationsController : BaseController
+    {
+        private readonly IOrganisationsService Organisations;
+
+        public OrganisationsController(IOrganisationsService organisations)
+        {
+            this.Organisations = organisations;
+        }
+
+        // GET: Administration/OrganisationsAdmin
+        public ActionResult Index()
+        {
+            var organisations = this.Organisations.GetAll()
+                .Where(x => x.IsDeleted == false)
+                .To<OrganisationViewModel>()
+                .ToList();
+
+            return View(organisations);
+        }
+
+        // GET: Administration/OrganisationsAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            var organisation = this.Organisations.GetAll()//.ToList().AsQueryable()
+                .To<OrganisationDetailsModel>()
+                .FirstOrDefault(x => x.Id == id);
+            if (organisation == null)
+            {
+                return this.HttpNotFound();
+            }
+            return this.View(organisation);
+        }
+
+        // GET: Administration/OrganisationsAdmin/Create
+        public ActionResult Create()
+        {
+            //ViewBag.OwnerId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Administration/OrganisationsAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,OwnerId,CreatedOn,ModifiedOn,IsDeleted,DeletedOn")] OrganisationCreateModel organisation)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Organisation()
+                {
+                    Name = organisation.Name,
+                    Description = organisation.Description,
+                    OwnerId = this.User.Identity.GetUserId()
+                };
+
+                this.Organisations.Add(entity);
+                this.Organisations.SaveChanges();
+                return this.RedirectToAction("Index");
+            }
+
+            //ViewBag.OwnerId = new SelectList(db.Users, "Id", "Email", organisation.OwnerId);
+            return View(organisation);
+        }
+
+        // GET: Administration/OrganisationsAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var organisation = this.Organisations.GetAll().To<OrganisationViewModel>().FirstOrDefault(o => o.Id == id);
+
+            if (organisation == null)
+            {
+                return HttpNotFound();
+            }
+
+            //ViewBag.OwnerId = new SelectList(db.Users, "Id", "Email", organisation.OwnerId);
+            return View(organisation);
+        }
+
+        // POST: Administration/OrganisationsAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,OwnerId,CreatedOn,ModifiedOn,IsDeleted,DeletedOn")] OrganisationEditModel organisation)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = this.Organisations.GetById(organisation.Id);
+                entity.Description = organisation.Description;
+                entity.Name = organisation.Name;
+
+                this.Organisations.Update(entity);
+                this.Organisations.SaveChanges();
+                return this.RedirectToAction("Index");
+            }
+
+            var result =
+                this.Organisations.GetAll().To<OrganisationViewModel>().FirstOrDefault(o => o.Id == organisation.Id);
+
+            return this.View(result);
+        }
+    }
+}

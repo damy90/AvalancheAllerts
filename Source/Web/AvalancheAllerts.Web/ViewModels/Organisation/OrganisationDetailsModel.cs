8e68ea999e9eb1997f0e9f004661ed71cb244880
damy90 <dany90@gmail.com>
2016-02-24 02:01:33 +0200

    Displaying users in organisation details

--- Source/Web/AvalancheAllerts.Web/ViewModels/Organisation/OrganisationDetailsModel.cs
+++ Source/Web/AvalancheAllerts.Web/ViewModels/Organisation/OrganisationDetailsModel.cs
@@ -8,6 +8,7 @@
 
     using AvalancheAllerts.Data.Models;
     using AvalancheAllerts.Web.ViewModels.Test;
+    using AvalancheAllerts.Web.ViewModels.User;
 
     public class OrganisationDetailsModel : OrganisationViewModel
     {
@@ -19,13 +20,16 @@
 
         public IEnumerable<TestViewModel> Tests { get; set; }
 
+        public IEnumerable<UserViewModel> Users { get; set; }
+
         public override void CreateMappings(IMapperConfiguration configuration)
         {
             configuration.CreateMap<Organisation, OrganisationDetailsModel>()
                 .ForMember(x => x.Owner, opt => opt.MapFrom(x => x.Owner.Email))
                 .ForMember(
                     x => x.Tests,
-                    opt => opt.MapFrom(x => x.Tests));
+                    opt => opt.MapFrom(x => x.Tests))
+                .ForMember(x => x.Users, opt => opt.MapFrom(x => x.Users));
         }
     }
 }
---
+++ Source/Web/AvalancheAllerts.Web/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace AvalancheAllerts.Web.ViewModels.User
+{
+    using AutoMapper;
+
+    using AvalancheAllerts.Data.Models;
+    using AvalancheAllerts.Web.Infrastructure.Mapping;
+
+    public class UserViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public int Tests { get; set; }
+
+        public int Organisations { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<ApplicationUser, UserViewModel>()
+                .ForMember(x => x.Organisations, opt => opt.MapFrom(x => x.Organisations.Count))
+                .ForMember(x => x.Tests, opt => opt.MapFrom(x => x.Tests.Count));
+        }
+    }
+}

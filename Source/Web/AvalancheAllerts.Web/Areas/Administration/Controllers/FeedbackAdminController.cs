d728d8af1fed95ffad0d38914180c941918a2c69
damy90 <dany90@gmail.com>
2016-02-24 09:57:52 +0200

    Adding FedbackAdminController

---
+++ Source/Web/AvalancheAllerts.Web/Areas/Administration/Controllers/FeedbackAdminController.cs
@@ -0,0 +1,59 @@
+﻿namespace AvalancheAllerts.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AvalancheAllerts.Common;
+    using AvalancheAllerts.Services.Data;
+    using AvalancheAllerts.Web.Controllers;
+    using AvalancheAllerts.Web.Infrastructure.Mapping;
+    using AvalancheAllerts.Web.ViewModels.Feedback;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class FeedbackAdminController : BaseController
+    {
+        private const int ItemsPerPage = 4;
+
+        private readonly IFeedbackService feedback;
+
+        public FeedbackAdminController(IFeedbackService feedback)
+        {
+            this.feedback = feedback;
+        }
+
+        [HttpGet]
+        public ActionResult Index(int id = 1)
+        {
+            FeedBackListViewModel viewModel;
+            if (this.HttpContext.Cache["Feedback page_" + id] != null)
+            {
+                viewModel = (FeedBackListViewModel)this.HttpContext.Cache["Feedback page_" + id];
+            }
+            else
+            {
+                var page = id;
+                var allItemsCount = this.feedback.GetAll().Count();
+                var totalPages = (int)Math.Ceiling(allItemsCount / (decimal)ItemsPerPage);
+                var itemsToSkip = (page - 1) * ItemsPerPage;
+                var feedbacks = this.feedback.GetAll()
+                    .OrderBy(x => x.CreatedOn)
+                    .ThenBy(x => x.Id)
+                    .Skip(itemsToSkip)
+                    .Take(ItemsPerPage)
+                    .To<FeedbackViewModel>().ToList();
+
+                viewModel = new FeedBackListViewModel()
+                {
+                    CurrentPage = page,
+                    TotalPages = totalPages,
+                    FeedBacks = feedbacks
+                };
+
+                this.HttpContext.Cache["Feedback page_" + id] = viewModel;
+            }
+
+            return this.View(viewModel);
+        }
+    }
+}
